2644ead0555fc448a929efd74a5d4dee7f0ef836
Ian Maina <muchemiian01@gmail.com>
2023-06-30 12:28:58 +0300

    ian

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,23 @@ import Log from "./log"
 import Signup from './sign'
 import Autentification from "./autentification"
 import Homepage from './home'
+import Navbar from './navbar'
 
 
 function App() {
   const [count, setCount] = React.useState(0)
-
+  const [darkMode, setDarkMode] = React.useState(true)
+    
+  function toggleDarkMode() {
+      setDarkMode(prevMode => !prevMode)
+  }
   return (
     <>
+    <Navbar  
+      darkMode={darkMode} 
+      toggleDarkMode={toggleDarkMode}/>
     <div className="fieldsflex">
+    
       <Log/>
       <Signup/>
       </div>
---
+++ src/navbar.jsx
@@ -0,0 +1,29 @@
+import React from "react"
+
+
+export default function Navbar(props) {
+    return (
+         <nav 
+            className={props.darkMode ? "dark": ""}
+        >
+            
+            
+            <h3 className="nav--logo_text">TechPulse</h3>
+            <input placeholder="Search..." type="text" name="text" class="input"></input>
+            <button className="Nav-Btn">Search</button>
+            
+            <div 
+                className="toggler" 
+            >
+                <p className="toggler--light">Light</p>
+                <div 
+                    className="toggler--slider"
+                    onClick={props.toggleDarkMode}
+                >
+                    <div className="toggler--slider--circle"></div>
+                </div>
+                <p className="toggler--dark">Dark</p>
+            </div>
+        </nav>
+    )
+}
